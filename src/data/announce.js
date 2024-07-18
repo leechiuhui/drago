18db946aa96574cd655f65836b08c1678aa87b99
Bestian Tang <bestian@gmail.com>
2024-07-19 03:21:43 +0800

    Update announce.js

--- src/data/announce.js
+++ src/data/announce.js
@@ -1,5 +1,5 @@
 export const announces = [
-  {date: '2024-07-18', text: '八月工作坊籌備中'},
+  {date: '2024-07-18', text: '八月工作坊籌備中：有興趣的朋友請先空出08/08, 08/22上午時段'},
   {date: '2024-07-18', text: '【從心出發】Line群組成立',
     h: 'https://line.me/ti/g/6n2qAsibyY'
   }
