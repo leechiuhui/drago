d3ce25d604fc612588e5b63be98dd7ce62370800
Bestian Tang <bestian@gmail.com>
2024-07-19 15:15:28 +0800

    add bg-audio

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,60 @@
 <template lang="pug">
-.hello
-  h1 
-    span.fat-only 歡迎來到
-    | 【從心出發】Playback劇場
-  h2.ui.header 最新消息
-  p.announce(v-for="(a, idx) in announces")
-    a(v-if="a.h", :href="a.h", target="_blank", rel="noopener norefferer") {{a.date}}：{{ a.text }} 
-    span(v-else-if="a.r", :to="a.r") {{a.date}}：{{ a.text }} 
-    span(v-else)  {{a.date}}：{{ a.text }} 
-  .ui.vertical.buttons
-    a.ui.large.purple.button(href="https://docs.google.com/document/d/17lp1gGyKwxt8gU64gMlOwPE-_p5lQeWaZ-8g8T7KsDU/edit?usp=sharing", target="_blank", rel="noopener norefferer") Playback精華
-    router-link.ui.large.pink.button(to="/about") 從流動塑像開始
-    a.ui.large.green.button(href="https://line.me/ti/g/6n2qAsibyY", target="_blank", rel="noopener norefferer") 【從心出發】Line群組
-
-  img.qr(src="../assets/heart_playback_qr.jpeg")
-
-</template>
-
-
-<script>
-
-import { announces } from '../data/announce.js'
-
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data () {
-    return {
-      announces: announces
+  .hello
+    h1 
+      span.fat-only 歡迎來到
+      | 【從心出發】Playback劇場
+    h2.ui.header 最新消息
+    p.announce(v-for="(a, idx) in announces")
+      a(v-if="a.h", :href="a.h", target="_blank", rel="noopener norefferer") {{a.date}}：{{ a.text }} 
+      span(v-else-if="a.r", :to="a.r") {{a.date}}：{{ a.text }} 
+      span(v-else)  {{a.date}}：{{ a.text }} 
+    .ui.vertical.buttons
+      a.ui.large.purple.button(href="https://docs.google.com/document/d/17lp1gGyKwxt8gU64gMlOwPE-_p5lQeWaZ-8g8T7KsDU/edit?usp=sharing", target="_blank", rel="noopener norefferer") Playback精華
+      router-link.ui.large.pink.button(to="/about") 從流動塑像開始
+      a.ui.large.green.button(href="https://line.me/ti/g/6n2qAsibyY", target="_blank", rel="noopener norefferer") 【從心出發】Line群組
+  
+    img.qr(src="../assets/heart_playback_qr.jpeg")
+    audio#background-music(src="/Shiva.m4a", autoplay, loop)
+  
+  </template>
+  
+  <script>
+  import { announces } from '../data/announce.js'
+  
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+        announces: announces
+      }
     }
   }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-.hello {
-  color: rgb(64, 3, 206);
-  height: 100vh;
-  padding-top: 2em;
-}
-
-p.announce {
-  font-size: 18px;
-  width: fit-content;
-  display: block;
-  margin: 1em auto;
-}
-
-img.qr {
-  width: 140px;
-  display: block;
-  margin: 1em auto;
-}
-
-</style>
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+  
+  .hello {
+    color: rgb(64, 3, 206);
+    height: 100vh;
+    padding-top: 2em;
+  }
+  
+  p.announce {
+    font-size: 18px;
+    width: fit-content;
+    display: block;
+    margin: 1em auto;
+  }
+  
+  img.qr {
+    width: 140px;
+    display: block;
+    margin: 1em auto;
+  }
+  
+  </style>
+  
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('audio')
+      .test(/\.(mp3|m4a|wav|flac)$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]'
+      });
+  },
   css: {
     loaderOptions: {
       sass: {
