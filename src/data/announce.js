0338487965ff0b66da9b193af07daa4fa6fab1b7
Bestian Tang <bestian@gmail.com>
2024-08-14 19:42:40 +0800

    Update announce.js

--- src/data/announce.js
+++ src/data/announce.js
@@ -1,8 +1,9 @@
 export const announces = [
+    {date: '2024-08-14', 
+        text: '【從心出發】九月工作坊09/14, 09/15開放報名中',
+        h: 'https://forms.gle/yquAWnrjzB1nuvys9'}
 //  {date: '2024-07-31', text: '八月工作坊已確認成行，延長報名至08/06', h: 'https://forms.gle/6yQVzVvWew6vQ1eH8'},
 //  {date: '2024-07-18', text: '八月工作坊籌備中：有興趣的朋友請先空出08/08, 08/22上午時段'},
-//  {date: '2024-07-18', text: '【從心出發】Line群組成立',
-//    h: 'https://line.me/ti/g/6n2qAsibyY'
-//  }
+
 
 ]
